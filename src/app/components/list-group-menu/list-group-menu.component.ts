8f9f7784d17c286f4f4bada8b4e2fdc7f50bc12c
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-03-30 18:27:07 +0200

    fancy app menu

---
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MenuEntry } from 'src/app/shared/interfaces/menu-entry';
+
+@Component({
+  selector: 'app-list-group-menu',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './list-group-menu.component.html',
+  styleUrl: './list-group-menu.component.css',
+})
+export class ListGroupMenuComponent {
+  menu: MenuEntry[] = [
+    { text: 'Component Input Example', routerLink: 'component-input-example' },
+    { text: '@for Directive Example', routerLink: 'for-directive-example' },
+    { text: 'Event Bind Example', routerLink: 'event-bind-example' },
+  ];
+}
