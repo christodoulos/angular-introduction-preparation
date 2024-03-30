3eab4a9063eb48ab6645fbdf217ac798a931b6e8
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-03-30 17:47:40 +0200

    other examples on menu

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -4,7 +4,7 @@ import { Person } from 'src/app/shared/interfaces/person';
 @Component({
   selector: 'app-person-table',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './person-table.component.html',
   styleUrl: './person-table.component.css',
 })
